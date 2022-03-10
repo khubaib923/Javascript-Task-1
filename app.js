6d5946d70f6cd5c355f941975ae81449f1d63796
khubaib923 <khubaibirfan199@gmail.com>
2022-03-10 23:03:17 +0500

    Completed Javacript Task

---
+++ app.js
@@ -0,0 +1,286 @@
+// ==============Task no 1====================
+          
+// alert("Aslam O Alikum Visitor");
+// alert('Error!Please enter a valid password.');
+// alert('Welcome to JS Land... \nHappy Coding!');
+// alert('Welcome to JS Land... ');
+// alert('Happy Coding \n Preventing this page from creating addditional dialogs');
+// alert('Hello... I can run JS through my web browser\'s console');
+//Body (inside your page’s HTML)
+
+// ==============Task no 02====================
+
+// var userName;
+// var myName="Muhammad Khuabib Irfan";
+// var message;
+// message="Hello World";
+// alert(message);
+
+// var studentName="Jhone Doe";
+// var studentAge="15 years old";
+// var studentProfession="Certified Mobile Applicaton Development";
+// alert(studentName);
+// alert(studentAge);
+// alert(studentProfession);
+
+
+// var item="PIZZA\nPIZZ\nPIZZ\nPIZ\nPI\nP";
+// alert(item);
+
+
+// var email="khubaibirfan199@gmail.com";
+// alert("My email address is " +email);
+
+
+// var book="A smarter\nway to learn JavaScript";
+// alert("I am trying to learn from the Book "+book);
+
+// document.write("Yah!I can write HTML content through JavaScript");
+
+// var pic="▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+// alert(pic);
+
+
+// ==============Task no 03====================
+
+// var age=22;
+// alert("I am " +age + " years old");
+
+// var trackDetail=14;
+// alert("You have visited the site " + trackDetail +" times");
+
+// var birthYear=1999;
+// document.write("My birth year is " +birthYear + `<br>`);
+// document.write("Data type of my declared variable is " +typeof birthYear);
+
+
+// var VisitorsName="<b>John Doe</b>";
+// var ProductTitle="<b>T-shirt(s)</b>";
+// var quantity="<b>5</b>";
+// document.write(VisitorsName+" ordered "+ quantity + "\n"+ProductTitle +" on XYZ Clothing store");
+
+// ==============Task no 04====================
+
+// var name,age,rollNo;
+
+//legal Variable Name
+
+// var name;
+// var fatherName;
+// var first_name;
+// var age1;
+// var age$;
+
+//Illegal Variable Name
+
+// var  _fatherName;
+// var $age;
+// var FatherNme;
+// var age$age;
+// var a1a;
+
+// document.write(`<b>Rules for naming JS variables</b>`);
+// document.write(`<br><br><br><br>`);
+// document.write("Variables name only contain,numbers,$ and _ .For example : $my_1stVariable"+`<br>`);
+// document.write("Variables name begin with a letter,$ or _ . For example : $name,_name or name" + `<br>`);
+// document.write("Variable names are case sensitive"+ `<br>` );
+// document.write("Variables name should not be JS keywords");
+
+
+// ==============Task no 05====================
+
+
+// var firstNumber=3;
+// var secondNumber=5;
+// var addition=firstNumber+secondNumber;
+// var subtarction=firstNumber-secondNumber;
+// var multiplication=firstNumber*secondNumber;
+// var divison=firstNumber/secondNumber;
+// var modulus=firstNumber%secondNumber;
+
+// document.write("Sum of "+firstNumber +" and " +secondNumber + " is " +addition +`<br>`);
+// document.write("Subtraction of "+firstNumber +" and " +secondNumber + " is " +subtarction +`<br>`);
+// document.write("Multiplication of "+firstNumber +" and " +secondNumber + " is " +multiplication +`<br>`);
+// document.write("Divison of "+firstNumber +" and " +secondNumber + " is " +divison +`<br>`);
+// document.write("Modulus of "+firstNumber +" and " +secondNumber + " is " +modulus);
+
+
+// var number;
+// document.write("Value after variable declaration is "+number +`<br>`);
+// number=5;
+// document.write("Initial value: "+number + `<br>`);
+// document.write("Value after increment is: " + ++number + `<br>`);
+// number+=7;
+// document.write("Value after addition is: " +number +`<br>`);
+// document.write("Value after decrement is: " + --number + `<br>`);
+// number%=3;
+// document.write("The remainder is: " +number);
+
+// var costMovieTicket=600;
+// var tickets=5;
+// document.write("Total cost to buy " +tickets + " tickets to a movie is " + tickets*costMovieTicket + " PKR");
+
+
+// var number=4;
+// document.write("Table of " +number + `<br><br>`);
+// document.write(number + " * 1 = " +number*1 + `<br>`);
+// document.write(number + " * 2 = " +number*2 + `<br>`);
+// document.write(number + " * 3 = " +number*3 + `<br>`);
+// document.write(number + " * 4 = " +number*4 + `<br>`);
+// document.write(number + " * 5 = " +number*5 + `<br>`);
+// document.write(number + " * 6 = " +number*6 + `<br>`);
+// document.write(number + " * 7 = " +number*7 + `<br>`);
+// document.write(number + " * 8 = " +number*8 + `<br>`);
+// document.write(number + " * 9 = " +number*9 + `<br>`);
+// document.write(number + " * 10 = " +number*10);
+
+
+// var celciusTemperature=25;
+// var  fahrenheitTemperature = (celciusTemperature*9/5)+32;
+// document.write(celciusTemperature +  "C " + " is " + fahrenheitTemperature +  "F" +`<br>`);
+
+// var fahrenheitTemperature1=70;
+// var celciusTemperature1=(fahrenheitTemperature1-32)*(5/9);
+// document.write(fahrenheitTemperature1 + "F " + " is " + celciusTemperature1 + "C");
+
+// var priceItem1=650;
+// var quantityItem1=3;
+// var priceItem2=100;
+// var quantityItem2=7;
+// var shippingCharges=100;
+// var totalCost=(quantityItem1*priceItem1)+(quantityItem2*priceItem2)+shippingCharges;
+
+// document.write("Price of Item 1 is " + priceItem1 + `<br>`);
+// document.write("Quantity of Item 1 is " + quantityItem1 + `<br>`);
+// document.write("Price of Item 2 is " + priceItem2 + `<br>`);
+// document.write("Quantity of Item 2 is " + quantityItem2 + `<br>`);
+// document.write("Shipping Charges " + shippingCharges + `<br>`);
+// document.write("Total cost of your order is " +totalCost);
+
+// var totalMarks=980;
+// var marks=804;
+// var percentage=(marks*100)/totalMarks;
+
+// document.write("<b>Marks Sheet<b>" +`<br><br><br>`);
+// document.write("Total Marks: " +totalMarks + `<br>`);
+// document.write("Marks Obtained: " +marks + `<br>`);
+// document.write("Percentage:" +percentage + "%");
+
+
+// var usDollars=10;
+// var saudiRiyals=25;
+// var convert=(usDollars*104.80)+(saudiRiyals*28);
+// document.write("<b>Currency in PKR</b>" + `<br>`);
+// document.write("Total currency in PKR: " +convert);
+
+
+// var number=5;
+// var expression=((number+5)*10)/2;
+// document.write("the result is " +expression);
+
+// var currentYear=2022;
+// var birthYear=1999;
+// var myAge=currentYear-birthYear;
+// document.write("my current age is: " +myAge);
+
+// var radius=20;
+// var circumstances=2*3.142*radius;
+// var area=3.142*radius*radius;
+// document.write("<b>The Geometrizer</b>" + `<br><br><br>`);
+// document.write("Radius of a circle: " +radius + `<br>`);
+// document.write("The circumstances is: " +circumstances + `<br>`);
+// document.write("The area is: " +area);
+
+
+// var favouriteSnack="chocolate chip";
+// var currentAge=15;
+// var maxAge=65;
+// var estimatedAmountPerDay=3;
+// var totalEat=(maxAge-currentAge)*3;
+// document.write("<b>The Lifetime Supply Calculator</b>" +`<br><br><br>`);
+// document.write("Favourite Snack: " +favouriteSnack + `<br>`);
+// document.write("Current age: " +currentAge + `<br>`);
+// document.write("Estimated Maximum Age: " +maxAge + `<br>`);
+// document.write("Amount off snacks per day: " +estimatedAmountPerDay + `<br>`);
+// document.write("You will need " +totalEat + " chocolate chip to last you until the ripe old age of " +maxAge);
+
+// ==============Task no 06====================
+
+
+// var a=10;
+// document.write("Result:" +`<br>`);
+// document.write("The value of a is: " +a +`<br>`);
+// document.write("<hr>");
+// document.write("The value of ++a is: " + ++a +`<br>`);
+// document.write("Now the value of a is: " + a +`<br><br>`);
+// document.write("The value of a++ is: " + a++ +`<br>`);
+// document.write("Now the value of a is: " + a +`<br><br>`);
+// document.write("The value of --a is: " + --a +`<br>`);
+// document.write("Now the value of a is: " + a +`<br><br>`);
+// document.write("The value of a-- is: " + a-- +`<br>`);
+// document.write("Now the value of a is: " + a +`<br><br>`);
+
+// var a=2;
+// var b=1;
+// // var result= --a - --b + ++b +b--;
+
+// var result1= --a;
+// document.write("a is: " +a + `<br>`);
+// document.write("b is: " +b + `<br>`);
+// document.write("result is: " +result1 + `<br>`);
+
+// document.write("<br>");
+// var result1= --a - --b;
+// document.write("a is: " +a + `<br>`);
+// document.write("b is: " +b + `<br>`);
+// document.write("result is: " +result1 + `<br>`);
+
+// document.write("<br>");
+
+// var result1= --a - --b + ++b;
+// document.write("a is: " +a + `<br>`);
+// document.write("b is: " +b + `<br>`);
+// document.write("result is: " +result1 + `<br>`);
+
+// document.write("<br>");
+
+// var result1= --a - --b + ++b + b--;
+// document.write("a is: " +a + `<br>`);
+// document.write("b is: " +b + `<br>`);
+// document.write("result is: " +result1 + `<br>`);
+
+// prompt("Please input a name:");
+// document.write("Asalam O alikum");
+
+
+//  var number=Number(prompt("Enter a number"));
+//  if(number != null){
+//  document.write("Table of " +number + `<br><br>`);
+//  for(var i=1;i<=10;i++){
+  
+//   document.write(number + "*" + i + "=" + number*i + `<br>`);
+//  }
+// }
+// else{
+//     var defaultNumber=5;
+//     document.write("Table of " +defaultNumber +`<br><br>`);
+//     for(var i=1;i<=10;i++){
+//         document.write(defaultNumber + "*" + i + "=" + number*i + `<br>`);
+
+//     }
+// }
+ 
+
+// var course1=prompt("Enter subject name");
+// var courseMarks1=Number(prompt("Enter " + course1 + " No"));
+// var course2=prompt("Enter subject name");
+// var courseMarks2=Number(prompt("Enter " + course2 + " No"));
+// var course3=prompt("Enter subject name");
+// var courseMarks3=Number(prompt("Enter " + course3 + " No"));
+// var obtainedMarks=courseMarks1+courseMarks2+courseMarks3;
+// console.log(obtainedMarks);
+
+// var totalMarks=300;
+// var percentage=(obtainedMarks*100)/totalMarks;
+// document.write("Obtained marks is: " +obtainedMarks + `<br>`);
+// document.write("Percentage is: " +percentage + "%");
